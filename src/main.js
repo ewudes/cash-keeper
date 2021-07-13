4b2fb9219297bf29d627ba10ca5f37a90497ca74
ewudes <ewudes@gmail.com>
2021-07-13 20:08:34 +0400

    adds validation for the login form

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue';
+import Vuelidate from 'vuelidate';
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
@@ -9,6 +10,7 @@ import 'materialize-css/dist/js/materialize.min';
 Vue.config.productionTip = false;
 
 Vue.filter('date', dateFilter);
+Vue.use(Vuelidate);
 
 new Vue({
   router,
