bffec562262d0a59ca05fc2f30de548483fa0996
ewudes <ewudes@gmail.com>
2021-07-21 15:47:30 +0400

    adds the display of the user name

--- src/store/auth.js
+++ src/store/auth.js
@@ -13,8 +13,9 @@ export default {
       }
     },
 
-    async logout() {
+    async logout({ commit }) {
       await firebase.auth().signOut();
+      commit('clearInfo');
     },
 
     async register({ dispatch, commit }, { email, password, name }) {
