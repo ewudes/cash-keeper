fb726f2d2bf2ea44522758d48a404b5e5b6b72c7
ewudes <ewudes@gmail.com>
2021-07-22 19:30:19 +0400

    adds the ability to edit categories

--- src/components/CategoryEdit.vue
+++ src/components/CategoryEdit.vue
@@ -5,26 +5,45 @@
         <h4>Редактировать</h4>
       </div>
 
-      <form>
-        <div class="input-field" >
-          <select>
-            <option>Category</option>
+      <form @submit.prevent="submitHandler">
+        <div class="input-field">
+          <select ref="select" v-model="current">
+            <option
+                v-for="c of categories"
+                :key="c.id"
+                :value="c.id"
+            >{{c.title}}
+            </option>
           </select>
           <label>Выберите категорию</label>
         </div>
 
         <div class="input-field">
-          <input type="text" id="name">
+          <input
+              id="name"
+              type="text"
+              v-model="title"
+              :class="{invalid: $v.title.$dirty && !$v.title.required}">
           <label for="name">Название</label>
-          <span class="helper-text invalid">TITLE</span>
+          <span
+              class="helper-text invalid"
+              v-if="$v.title.$dirty && !$v.title.required">
+            Введите название категории
+          </span>
         </div>
 
         <div class="input-field">
           <input
               id="limit"
-              type="number" >
+              type="number"
+              v-model.number="limit"
+              :class="{invalid: $v.limit.$dirty && !$v.limit.minValue}">
           <label for="limit">Лимит</label>
-          <span class="helper-text invalid">LIMIT</span>
+          <span
+              class="helper-text invalid"
+              v-if=" $v.limit.$dirty && !$v.limit.minValue">
+            Минимальная величина {{$v.limit.$params.minValue.min}}
+          </span>
         </div>
 
         <button class="btn waves-effect waves-light" type="submit">
@@ -35,3 +54,75 @@
     </div>
   </div>
 </template>
+
+<script>
+import { required, minValue } from 'vuelidate/lib/validators';
+
+export default {
+  props: {
+    categories: {
+      type: Array,
+      required: true,
+    },
+  },
+  data: () => ({
+    select: null,
+    title: '',
+    limit: 100,
+    current: null,
+  }),
+  validations: {
+    title: { required },
+    limit: { minValue: minValue(100) },
+  },
+  watch: {
+    current(catId) {
+      const { title, limit } = this.categories.find((c) => c.id === catId);
+      this.title = title;
+      this.limit = limit;
+    },
+  },
+  created() {
+    setTimeout(() => {
+      const { id, title, limit } = this.categories[0];
+      this.current = id;
+      this.title = title;
+      this.limit = limit;
+    }, 200);
+  },
+  methods: {
+    async submitHandler() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        // eslint-disable-next-line no-useless-return
+        return;
+      }
+      try {
+        const categoryData = {
+          id: this.current,
+          title: this.title,
+          limit: this.limit,
+        };
+        await this.$store.dispatch('updateCategory', categoryData);
+        this.$message('Категория успешно обновлена');
+        this.$emit('updated', categoryData);
+      } catch (e) {
+        console.log(e);
+      }
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      // eslint-disable-next-line no-undef
+      this.select = M.FormSelect.init(this.$refs.select);
+      // eslint-disable-next-line no-undef
+      M.updateTextFields();
+    }, 200);
+  },
+  destroyed() {
+    if (this.select && this.select.destroy) {
+      this.select.destroy();
+    }
+  },
+};
+</script>
--- src/views/Categories.vue
+++ src/views/Categories.vue
@@ -4,9 +4,17 @@
       <h3>Категории</h3>
     </div>
     <section>
+      <Loader v-if="loading"/>
       <div class="row">
-        <CategoryCreate @created="addNewCategory"/>
-        <CategoryEdit/>
+        <CategoryCreate
+          @created="addNewCategory"/>
+
+        <CategoryEdit
+          v-if="categories.length"
+          @updated="updateCategories"
+          :key="categories.length + updateCount"
+          :categories="categories"/>
+        <p v-else class="center">Категорий пока нет</p>
       </div>
     </section>
   </div>
@@ -20,13 +28,27 @@ export default {
   name: 'categories',
   data: () => ({
     categories: [],
+    loading: true,
+    updateCount: 0,
   }),
+  async mounted() {
+    this.categories = await this.$store.dispatch('fetchCategories');
+    console.log(this.categories);
+    this.loading = false;
+  },
   components: {
     CategoryCreate, CategoryEdit,
   },
   methods: {
     addNewCategory(category) {
       this.categories.push(category);
+    },
+    updateCategories(category) {
+      const idx = this.categories.findIndex((c) => c.id === category.id);
+      this.categories[idx].title = category.title;
+      this.categories[idx].limit = category.limit;
+      // eslint-disable-next-line no-plusplus
+      this.updateCount++;
     },
   },
 };
