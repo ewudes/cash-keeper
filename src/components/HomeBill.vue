dfcf760683786f01ad11b68767c547cdd04afd0b
ewudes <ewudes@gmail.com>
2021-09-02 22:06:21 +0400

    fix: small style changes

--- src/components/HomeBill.vue
+++ src/components/HomeBill.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="col s12 m6 l4">
-    <div class="card light-blue bill-card">
-      <div class="card-content white-text">
+    <div class="card bill-card">
+      <div class="card-content">
         <span class="card-title">Счет в валюте</span>
 
         <p
